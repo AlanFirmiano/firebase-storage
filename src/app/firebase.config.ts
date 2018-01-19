abca8027f8bc2fef847341ad07c84124ba7a58c9
alanfirmiano <alanfirmiano18@gmail.com>
2018-01-19 14:48:49 -0300

    firebase config add

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAAKvEoa8HIyqPS0rx7N2m0x9FxqGsYx2E",
+  authDomain: "fir-storage-ionic-c3747.firebaseapp.com",
+  databaseURL: "https://fir-storage-ionic-c3747.firebaseio.com",
+  projectId: "fir-storage-ionic-c3747",
+  storageBucket: "fir-storage-ionic-c3747.appspot.com",
+  messagingSenderId: "695980560449"
+};
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { storage, initializeApp } from 'firebase';
+import {FIREBASE_CONFIG} from "../../app/firebase.config";
+import {Camera, CameraOptions} from "@ionic-native/camera";
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    private camera: Camera
+  ) {
+    initializeApp(FIREBASE_CONFIG);
   }
 
+  async takePhoto(){
+    try {
+      const options: CameraOptions = {
+        quality: 50,
+        targetHeight: 600,
+        targetWidth: 600,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
+      }
+      const result = await this.camera.getPicture(options);
+
+      const image = `data:image/jpeg;base64,${result}`;
+
+      const pictures = storage().ref('pictures');
+      pictures.putString(image, 'data_url');
+    }catch (e){
+
+    }
+  }
 }
